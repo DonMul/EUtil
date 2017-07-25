061e3e252bf93ba3e3423596c41ae4afcd216bb1
Joost Mul <joost@jmul.net>
2017-07-25 17:00:10 +0200

    Added a EUtil\Http class

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # EUtil (Extended Utils)
 ## What is it?
 It is just a collection of handy functionality that was made in the passing of time. Usually i make these for some reason and just add the functionality to this library.
+
+## I want a function in this
+Sure, just let me know and i'll try to add it (or you can submit a pull request).
---
+++ src/Http.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace EUtil;
+
+/**
+ * Class Http
+ * @author Joost Mul <eutil@jmul.net>
+ */
+final class Http
+{
+    /**
+     * @param string $url
+     * @return string
+     */
+    public function getPageContentForUrl($url)
+    {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return $result;
+    }
+}
